7a7d528f8544040cd1a16d334901dabc17c85596
axel-paillaud <contact@axelweb.fr>
2024-08-22 11:43:25 +0200

    make some use example of template and slot

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import "./style.css";
 import viteLogo from "/vite.svg";
+
+customElements.define(
+  "my-paragraph",
+  class extends HTMLElement {
+    constructor() {
+      super();
+      let template = document.getElementById("my-paragraph");
+      let templateContent = template.content;
+
+      const shadowRoot = this.attachShadow({ mode: "open" }).appendChild(
+        templateContent.cloneNode(true),
+      );
+    }
+  },
+);
+
+let app = document.getElementById("app");
+let myParagraph = document.createElement("my-paragraph");
+
+app.appendChild(myParagraph);
