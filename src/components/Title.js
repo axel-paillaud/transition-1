17b436773d09eac5db86eede2607a8d4f9481635
axel-paillaud <contact@axelweb.fr>
2024-08-27 10:54:05 +0200

    base title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,10 +2,18 @@ const template = document.createElement("template");
 
 template.innerHTML = `
 <style>
-
+.title {
+    font-family: Branch;
+    margin: 0;
+    font-size: 128px;
+    color: var(--grass-500);
+    position: absolute;
+    left: 64px;
+    top: 0;
+}
 </style>
 
-<h1>Allemagne</h1>
+<h1 class="title">Allemagne</h1>
 `
 
 export default class Title extends HTMLElement {
