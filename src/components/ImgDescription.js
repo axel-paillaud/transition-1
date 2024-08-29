96d137cdd65f671dc8ee15b8be5da02616e90be4
axel-paillaud <contact@axelweb.fr>
2024-08-29 11:31:23 +0200

    setup desc animation

--- src/components/ImgDescription.js
+++ src/components/ImgDescription.js
@@ -4,6 +4,7 @@ template.innerHTML = `
 <style>
 p {
     margin: 0;
+    color: var(--grass-500);
 }
 
 .description-container {
@@ -12,11 +13,37 @@ p {
     flex-direction: column;
     gap: 16px;
 }
+
+.container-mask {
+    overflow: hidden;
+}
+
+.item-1, .item-2 {
+    position: relative;
+    top: 18px;
+}
+
+.item-1 {
+    animation: 0.8s slide-up 1s cubic-bezier(.17,.84,.44,1) forwards;
+}
+
+.item-2 {
+    animation: 0.8s slide-up 1.2s cubic-bezier(.17,.84,.44,1) forwards;
+}
+
+@keyframes slide-up {
+    from { top: 18px; }
+    to { top: 0px; }
+}
 </style>
 
 <div class="description-container">
-    <p>Axel & Leila - juillet 2020</p>
-    <p>Lac de Plitvice</p>
+    <div class="container-mask">
+        <p class="item-1">Axel & Leila - juillet 2020</p>
+    </div>
+    <div class="container-mask">
+        <p class="item-2">Lac de Plitvice</p>
+    </div>
 </div>
 `
 
