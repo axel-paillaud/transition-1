61f0d09a89a1e5718cbcf095026d1a8c2cefc4c8
axel-paillaud <contact@axelweb.fr>
2024-09-03 11:28:47 +0200

    update country data

--- src/data/country.js
+++ src/data/country.js
@@ -3,10 +3,22 @@ import austriaPhoto from "/images/austria-768.avif";
 
 export const countryObj = {
     germany: {
-        id: 1, name: "germany", frName: "Allemagne", photo: germanyPhoto,
+        id: 1, 
+        name: "germany", 
+        frName: "Allemagne", 
+        photo: germanyPhoto,
+        title: 'Sarah & Louis',
+        date: 'juillet 2020',
+        description: 'Lorem ipsum dolor sit amet',
     },
     austria: {
-        id: 2, name: "austria", frName: "Autriche", photo: austriaPhoto,
+        id: 2, 
+        name: "austria", 
+        frName: "Autriche", 
+        photo: austriaPhoto,
+        title: 'Sarah & Louis',
+        date: 'août 2021',
+        description: 'Donec non lorem molestie',
     }
 };
 
